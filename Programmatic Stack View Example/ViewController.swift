d0d23b94301ae5ae87ac50224c2cab001f4a035e
Grant Elliott <gelliott@acumensolutions.com>
2018-09-12 15:41:35 -0400

    Programmatic Stack View Example

--- Programmatic Stack View Example/ViewController.swift
+++ Programmatic Stack View Example/ViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let colorDict: [String : UIColor] = [
+        "Red": UIColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 1.0),
+        "Green": UIColor(red: 0.0, green: 1.0, blue: 0.0, alpha: 1.0),
+        "Blue": UIColor(red: 0.0, green: 0.0, blue: 1.0, alpha: 1.0)
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        displayButtons()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,48 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    public func colorButton(withColor color: UIColor, title: String) -> UIButton {
+        let button = UIButton(type: .system)
+        
+        button.backgroundColor = color
+        button.setTitle(title, for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        
+        return button
+    }
+    
+    public func displayButtons() {
+        let buttonArray: [UIButton] = self.colorDict.reduce([]) { (acc, colorTuple) in
+            let button = colorButton(withColor: colorTuple.value, title: colorTuple.key)
+            let result = [button] + acc
+            
+            return result
+        }
+        
+        let stackView = buildStackView(subviews: buttonArray)
+        self.view.addSubview(stackView)
+        
+        let margins = self.view.safeAreaLayoutGuide
+        stackView.leadingAnchor.constraint(equalTo: margins.leadingAnchor).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: margins.trailingAnchor).isActive = true
+        stackView.topAnchor.constraint(equalTo: margins.topAnchor).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: margins.bottomAnchor).isActive = true
+        
+    }
+    
+    private func buildStackView(subviews: [UIView]) -> UIStackView {
+        let stackView = UIStackView(arrangedSubviews: subviews)
+        
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.alignment = .fill
+        stackView.spacing = 5
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return stackView
+    }
+    
+    
 }
 
---
+++ Programmatic Stack View Example/Views/StackView.swift
@@ -0,0 +1,17 @@
+//
+//  StackView.swift
+//  Programmatic Stack View Example
+//
+//  Created by Grant Elliott on 9/12/18.
+//  Copyright © 2018 Grant Elliott. All rights reserved.
+//
+
+import UIKit
+
+class StackView: UIStackView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        
+    }
+}
